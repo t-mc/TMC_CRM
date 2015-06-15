d47159e8f39971b89cd06c1ac7e3ae33693e1354
t-mc <jaap.glasbergen@t-mc.nl>
2015-06-15 16:08:04 +0200

    2e

---
+++ T-MC CRM/T-MC CRM.HTMLClient/Screens/Bedrijvens/ViewBedrijven.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewBedrijven.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Bedrijven.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Bedrijven." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
---
+++ T-MC CRM/T-MC CRM.HTMLClient/Screens/Contactpersonens/ViewContactpersonen.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewContactpersonen.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Contactpersonen.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Contactpersonen." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
