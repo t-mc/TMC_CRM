be55eae34dff385eb7b936427fe8740e6612bf4d
t-mc <jaap.glasbergen@t-mc.nl>
2015-06-18 12:25:39 +0200

    5

---
+++ T-MC CRM/T-MC CRM.HTMLClient/Screens/VerkoopkansenProjectens/AddEditVerkoopkansenProjecten.lsml.js
@@ -0,0 +1,42 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+var VerkoopStadium;
+
+myapp.AddEditVerkoopkansenProjecten.created = function (screen) {
+    // Write code here.
+    // alert("Koekoek 1");
+    VerkoopStadium = screen.VerkoopkansenProjecten.Verkoopstadium;
+};
+
+myapp.AddEditVerkoopkansenProjecten.beforeApplyChanges = function (screen) {
+    // Write code here.
+    // alert("Koekoek 2");
+    if (screen.VerkoopkansenProjecten.Verkoopstadium != VerkoopStadium) {
+        switch (screen.VerkoopkansenProjecten.Verkoopstadium) {
+            case "Marktkans":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0;
+                break;
+            case "Lead":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0.1;
+                break;
+            case "Prospect":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0.25;
+                break;
+            case "Verkoopkans":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0.5;
+                break;
+            case "Offerte":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0.75;
+                break;
+            case "Order":
+                screen.VerkoopkansenProjecten.Verkoopkans = 1;
+                break;
+            case "Order afgerond":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0;
+                break;
+            case "Loss":
+                screen.VerkoopkansenProjecten.Verkoopkans = 0;
+                break;
+
+            }
+    }
+};
--- T-MC CRM/T-MC CRM.HTMLClient/Screens/VerkoopkansenProjectens/ViewVerkoopkansenProjecten.lsml.js
+++ T-MC CRM/T-MC CRM.HTMLClient/Screens/VerkoopkansenProjectens/ViewVerkoopkansenProjecten.lsml.js
@@ -8,3 +8,22 @@ myapp.ViewVerkoopkansenProjecten.Details_postRender = function (element, content
     });
 }
 
+
+
+myapp.ViewVerkoopkansenProjecten.DeleteVerkoopkans_execute = function (screen) {
+    // Write code here.
+    var bevestig = confirm("Weet je zeker dat je deze contactpersoon wilt verwijderen?");
+
+    if (bevestig == true) {
+        screen.VerkoopkansenProjecten.deleteEntity();
+        return myapp.commitChanges().then(null, function fail(e) {
+            myapp.cancelChanges();
+            throw e;
+        });
+    }
+};
+myapp.ViewVerkoopkansenProjecten.Verkoopkans_render = function (element, contentItem) {
+    // Write code here.
+    var verkoopKans = $("<p><i>" + contentItem.value * 100 + " %</i></p>");
+    verkoopKans.appendTo($(element));
+};
