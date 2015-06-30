0437c16405caf5e6436b6b4b1371f484d8da37d2
t-mc <jaap.glasbergen@t-mc.nl>
2015-06-30 09:06:33 +0200

    30-6 9 uur

--- T-MC CRM/T-MC CRM.HTMLClient/Screens/Contactpersonens/AddEditContactpersonen.lsml.js
+++ T-MC CRM/T-MC CRM.HTMLClient/Screens/Contactpersonens/AddEditContactpersonen.lsml.js
@@ -16,34 +16,34 @@ myapp.AddEditContactpersonen.beforeApplyChanges = function (screen) {
 
 myapp.AddEditContactpersonen.created = function (screen) {
     // Write code here.
-    alert("Koekoek");
     // Zet default tekst als bedrijfsnaam leeg is.
-//     screen.findContentItem("Volledigenaam")
-//    .dataBind("value.selectedItem", function (newValue) {
-//        if (newValue == screen.Contactpersonen.VolledigeNaam) {
-//            screen.VerkoopkansenProjecten.VolledigeNaam = "< voeg bedrijf in >";
-//        }
-//    });
+     screen.findContentItem("VolledigeNaam")
+    .dataBind("value.selectedItem", function (newValue) {
+        if (newValue == screen.Contactpersonen.VolledigeNaam) {
+            screen.Contactpersonen.VolledigeNaam = "< voeg bedrijf in >";
+        }
+    });
     screen.Contactpersonen.VolledigeNaam = "Deze niet invullen, wordt automatisch gevuld!";
+
     // Zet default tekst als bedrijfsnaam leeg is.
     screen.findContentItem("Bedrijfsnaam")
     .dataBind("value.selectedItem", function (newValue) {
         if (newValue == screen.Contactpersonen.Bedrijfsnaam) {
-            screen.VerkoopkansenProjecten.Bedrijfsnaam = "< voeg bedrijf in >";
+            screen.Contactpersonen.Bedrijfsnaam = "< voeg bedrijf in >";
         }
     });
     // Zet default tekst als assistent leeg is.
     screen.findContentItem("Assistent")
     .dataBind("value.selectedItem", function (newValue) {
         if (newValue == screen.Contactpersonen.Assistent) {
-            screen.VerkoopkansenProjecten.Opdrachtgever = "< voeg assistent in >";
+            screen.Contactpersonen.Assistent = "< voeg assistent in >";
         }
     });
     // Zet default tekst als manager leeg is.
     screen.findContentItem("Manager")
     .dataBind("value.selectedItem", function (newValue) {
         if (newValue == screen.Contactpersonen.Manager) {
-            screen.VerkoopkansenProjecten.Opdrachtgever = "< voeg manager in >";
+            screen.Contactpersonen.Manager = "< voeg manager in >";
         }
     });
 };
